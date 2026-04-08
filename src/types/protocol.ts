9294e1cddf36bdc9f775e5d22780e52b3bc8f953
hriczzoli <6625360+hriczzoli@users.noreply.github.com>
2026-04-08 12:51:00 +0000

    refactor(types): sync embedded types

--- src/types/protocol.ts
+++ src/types/protocol.ts
@@ -2,10 +2,7 @@
 
 export type APIVersion = "v1";
 
-export type MessageType =
-  | "CORTI_EMBEDDED"
-  | "CORTI_EMBEDDED_RESPONSE"
-  | "CORTI_EMBEDDED_EVENT";
+export type MessageType = "CORTI_EMBEDDED" | "CORTI_EMBEDDED_RESPONSE" | "CORTI_EMBEDDED_EVENT";
 
 export type DefaultMode = "virtual" | "in-person";
 
