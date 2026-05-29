f1c8faca020b546e88b8da07e42b601592b5bc3d
Morten Barklund <mba@corti.ai>
2026-05-29 10:27:01 +0200

    test: cover React API hook pass-throughs

--- test/vendor/react-test-bundle.d.ts
+++ test/vendor/react-test-bundle.d.ts
@@ -1,5 +1,7 @@
 export const React: typeof import('react');
 export const createRoot: typeof import('react-dom/client').createRoot;
 export const CortiEmbeddedReact: typeof import('../../src/react/CortiEmbeddedReact.js').CortiEmbeddedReact;
+export const useCortiEmbeddedApi: typeof import('../../src/react/CortiEmbeddedReact.js').useCortiEmbeddedApi;
 export type Root = import('react-dom/client').Root;
 export type CortiEmbeddedReactRef = import('../../src/react/CortiEmbeddedReact.js').CortiEmbeddedReactRef;
+export type UseCortiEmbeddedApiResult = import('../../src/react/CortiEmbeddedReact.js').UseCortiEmbeddedApiResult;
